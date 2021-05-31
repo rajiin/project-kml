a65a72159438e1b721fe3bc661ab66c09a24f305
rajiin <ss@siqq.dk>
2021-05-31 13:11:08 +0200

    updates

--- php/kml2.php
+++ php/kml2.php
@@ -1,5 +1,9 @@
 <?php
 
+// google maps does not support HTML tags in description fields
+// google earth supports more tags
+// pictures are copied to the users drive account
+
 function kml($data)
 {
 printf("<?xml version='1.0' encoding='UTF-8'?>");
@@ -7,7 +11,7 @@ printf("<kml xmlns='http://www.opengis.net/kml/2.2' xmlns:gx='http://www.google.
 
     printf("<Document>\n");
 
-// create map for layers
+// create map layer for path
     printf("<Folder>\n");
     printf("<name>Sailserver Track</name>\n");
 
@@ -23,12 +27,13 @@ printf("<kml xmlns='http://www.opengis.net/kml/2.2' xmlns:gx='http://www.google.
     printf("<coordinates>\n");
 
 // all points from the track goes here to draw the path
+    printf("9.813954,54.895541,0 9.813902,54.895522,0 9.814396,54.895756,0 9.81675,54.896335,0 9.817199,54.896505,0");
 
     printf("</coordinates>\n");
     printf("</LineString>\n");
     printf("</Placemark>\n");
     printf("</Folder>\n");
-// end creating map and path layer
+// end creating path layer
 
 // create layer for all track points
     printf("<Folder>\n");
