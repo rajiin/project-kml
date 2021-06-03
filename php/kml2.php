3362f4df702a9a0f6bf197e2cc9a9dfc6d160646
rajiin <ss@siqq.dk>
2021-06-03 14:06:49 +0200

    updates

--- php/kml2.php
+++ php/kml2.php
@@ -1,19 +1,23 @@
 <?php
 
-// google maps does not support HTML tags in description fields
-// google earth supports more tags
-// pictures are copied to the users drive account
-
 function kml($tracks)
 {
-$data=$tracks[0]['points'];
+
+$points=$tracks[0]['points'];
+$track=$tracks[0]['track'];
+//print_r($tracks);
 
 printf("<?xml version='1.0' encoding='UTF-8'?>");
 printf("<kml xmlns='http://www.opengis.net/kml/2.2' xmlns:gx='http://www.google.com/kml/ext/2.2' xmlns:atom='http://www.w3.org/2005/Atom'>\n");
 
 printf("<Document>\n");
-printf("<name>Sailserver Project Name</name>");
-printf("<description>Sailserver Project Desciption</desription>");
+printf("<name>Sailserver Project Name</name>\n");
+printf("<description>");
+//    printf("Sailserver track from ");
+//    printf("%s",$track,['startname']);
+//    printf(" to ");
+//    printf("%s",$track,['stopname']);
+printf("</description>");
 
 // Style definitions
 printf("<LineStyle id='blueLine-01'>");
@@ -25,36 +29,39 @@ printf("</LineStyle>");
 // create map layer for path
     printf("<Folder>\n");
     printf("<name>Sailserver Track ");
-        printf("%s",$data['trackid']);
+        printf("%s",$track,['trackid']);
     printf("</name>\n");
 
 // create layer for the track path
     printf("<Placemark>\n");
     printf("<name>Track</name>\n");
-    printf("<styleURL>#blueLine-01</styleURL>");
-    printf("<LineString>");
-//        printf("<extrude>1</extrude>");
-        printf("<tessellate>1</tessellate>");
+    printf("<styleURL>#blueLine-01</styleURL>\n");
+    printf("<LineString>\n");
+        printf("<tessellate>1</tessellate>\n");
 
 // create all ponts in the track path
     printf("<coordinates>\n");
 
-foreach($data as $i => $t) {
+  foreach($tracks as $ti => $tr) {
+    foreach($tr['points'] as $i => $t) {
               printf("%s,%s,0\n",$t['lon'],$t['lat']);
-}
+    }
+  }
     printf("</coordinates>\n");
-    printf("</LineString>");
+    printf("</LineString>\n");
     printf("</Placemark>\n");
-    printf("</Folder>\n");
+    
+//    printf("</Folder>\n");
 // end creating path layer
 
 // create layer for all track points
-    printf("<Folder>\n");
- // layer name for all track points 
-    printf("<name>Points</name>\n");
+//    printf("<Folder>\n");
+// layer name for all track points 
+//    printf("<name>Points</name>\n");
 
-foreach($data as $i => $t) {
-    printf("<Placemark>\n");
+  foreach($tracks as $ti => $tr) {
+    foreach($tr['points'] as $i => $t) {
+      printf("<Placemark>\n");
     // name for the individual track points
         printf("<name>");
             printf("%s",$t['utc']);
@@ -65,7 +72,6 @@ foreach($data as $i => $t) {
             foreach($t as $j => $k) {
                 printf("<b>%s:</b> %s<br>",$j,$k);
             }
-
             printf("]]>");
         printf("</description>\n");
             
@@ -74,8 +80,9 @@ foreach($data as $i => $t) {
             printf("%s,%s,0",$t['lon'],$t['lat']);
             printf("</coordinates>\n");
         printf("</Point>\n");
-    printf("</Placemark>\n");
-}
+      printf("</Placemark>\n");
+    }
+  }
 // above repeats for all track points   
     
         printf("</Folder>\n");
